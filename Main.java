bc2c6814ee5f99d075584f92363c8f45809b3d53
mesaym <mesaymekonen77@gmail.com>
2025-08-25 15:41:08 +0300

    Remove even numbers fromn the given array

--- Main.java
+++ Main.java
@@ -1,23 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        Main main = new Main();
-        long now = System.currentTimeMillis(); // get current time in milli seconds
-        System.out.println(main.findSum(99999));
-        System.out.println(System.currentTimeMillis() - now); // get the time taken to run the algorithm
+        int[] numbers = {5,3,6,4,7,1,9,8,2};
 
-        // we can  check the time time taken by each algorithm by commenting out one of the algorithms at a time
+        int[] odds = removeEvenIntegers(numbers);
+        for (int i = 0; i<odds.length; i++){
+            System.out.println(odds[i]);
+        }
+
     }
 
-//    public int findSum(int n){
-//        return n * (n + 1);
-//    }
-
-
-    public int findSum(int n){
-        int sum = 0;
-        for (int i = 1; i<=n; i++){
-            sum += i;
+    public static int[] removeEvenIntegers(int[] myArray){
+        //get a count of odd numbers in the array to create an array with the length of the odd count
+        int oddCount = 0;
+        for (int  i = 0; i<myArray.length; i++){
+            if (myArray[i] % 2 != 0){
+                oddCount++;
+            }
         }
-        return sum;
+
+        int[] result = new int[oddCount]; // create an array with a length of odd count
+        int idx = 0;
+        for (int i = 0; i<myArray.length ; i++){
+            if (myArray[i] % 2 != 0){  // filter the odd numbers and assign
+                result[idx] = myArray[i]; // assigned the filtered odd number to the index of the result
+                idx++;
+            }
+        }
+        return result;
     }
 }
