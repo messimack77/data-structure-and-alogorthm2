cf95a5c86e7eae6515b7c171bd0c5948d02750c3
mesaym <mesaymekonen77@gmail.com>
2025-08-28 08:33:56 +0300

    Fix: move all zeros to end

--- MoveAllZerosToTheEnd.java
+++ MoveAllZerosToTheEnd.java
@@ -1,11 +1,10 @@
 public class MoveAllZerosToTheEnd {
 
 
-
     public static void main(String[] args) {
-        int[] array = {0,8,0,0,2,1,0,3,0};
+        int[] array = {0, 8, 0, 0, 2, 1, 0, 3, 0};
         int[] movedZeros = moveAllZerosToTheEnd(array);
-        for (int i : movedZeros){
+        for (int i : movedZeros) {
             System.out.println(i);
         }
 
@@ -15,17 +14,17 @@ public class MoveAllZerosToTheEnd {
 
         //0,8,5,0,2,1,0,3,0
         int j = 0; // tracks a position where the next zero element should go
-        for(int i = 0; i<numbers.length; i++){
-            if (numbers[i] != 0 && numbers[j] == 0){
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] != 0 && numbers[j] == 0) {
                 int temp = numbers[i];
                 numbers[i] = numbers[j];
                 numbers[j] = temp;
             }
+            if (numbers[j] != 0){
+                j++;
+            }
         }
-
+      return numbers;
     }
-
-
-
 
 }
